bd366566bf1ed2c511f99b023cc776667a59d34c
BT_1N3_20 <BT_1N3_20@BT_1N3_20.bktech.local>
2018-02-09 09:23:14 -0500

    asdsdf

--- Lab3.2/src/MergePartition.java
+++ Lab3.2/src/MergePartition.java
@@ -5,9 +5,9 @@ public class MergePartition {
 	public static void main(String[] args) {
 		
 		//Testing arrays
-		int [] test1 = {1,4,4,6,2,4,3,17,0};
-		String [] test2 = {"A", "C", "E" , "F" , "H" , "Z"};
-		String [] test3 = {"B" , "D" , "G" , "I", "J", "K" , "Y", "X" ,"Z"};
+		int [] test1 = {5,4,4,7,2,6} ; 
+		String [] test2 = {"A", "C", "E" , "F" , "H" , "X", "Y" ,"Z"};
+		String [] test3 = {"B" , "D" , "G" , "I", "J", "K" , "X", "Y" ,"Z" , "Z"};
 		
 		//MergeTest
 		long start = System.nanoTime();
@@ -15,8 +15,13 @@ public class MergePartition {
 		long end = System.nanoTime();
 		long time = end -start; 
 		System.out.println("Merge: " + time + "nanoseconds");
-				
 		
+		//PartitionTest
+		 start = System.nanoTime();
+		partition(test1);
+		end = System.nanoTime();
+		time = end -start; 
+		System.out.println("Partition: " + time + "nanoseconds");
 	
 		
 		
@@ -34,53 +39,43 @@ public class MergePartition {
 	Returns: An array of the two lists merged together. The result must be in alphabetical order. */
 	public static String[] merge(String[] list1 , String[] list2) {
 		String[] list3 = new String[list1.length+list2.length];
-		int index1 = 0 ; int index2 = 0 ; int i= 0 ;
+		int index1 = 0 ; int index2 = 0 ;
 		if (list1.length >= list2.length)
 		{
 
 			System.out.println("This works");
-			while (index1 != list1.length-1)
+			for ( int i =0 ; i < list3.length ; i++)
 			{
-				
-				 if (list1[index1].compareTo(list2[index2]) < 0 || list1[index1].compareTo(list2[index2])==0  )
+				if ( index1 > list1.length-1) {
+					list3[i] = list2[index2] ; 
+					index2++ ; 
+					
+					 continue ; }
+				if ( index2 > list2.length-1) {
+					list3[i] = list1[index1] ; 
+					index1++ ;
+					
+					 continue ; }
+				else if (list1[index1].compareTo(list2[index2]) < 0 || list1[index1].compareTo(list2[index2])==0  )
 					{
 					list3[i] = list1[index1] ; 
-					index1++ ;  i++ ; 
-					System.out.println(index1 + " index1"); 
+					index1++ ;
+					
 					}
-				 if ( index2 == list2.length+1) {
-					 continue ; 
-				 }
-				 if (list1[index1].compareTo(list2[index2]) > 0  )
+				 
+				 
+				 else if (list1[index1].compareTo(list2[index2]) > 0  )
 					{
 					list3[i] = list2[index2] ; 
-					index2++ ; i++ ;
-					System.out.println(index2 + " index2");
+					index2++ ;
+					
 					}
 				
 			} 
 			
 		}
 		
-/*		else if (list2.length >= list1.length)
-		{
-			System.out.println("This works");
-			for (int i= 0 ; i < list2.length ; i++)
-			{
-				if (list1[index1].compareTo(list2[index2]) == 1  )
-					{
-					list3[i] = list2[index2] ; 
-					index2++ ;
-					System.out.println(index2);
-					}
-				else if (list1[index1].compareTo(list2[index2]) == -1  )
-					{
-					list3[i] = list1[index1] ; 
-					index1++ ;  
-					System.out.println(index1);
-					}
-			}
-		} */
+
 		System.out.println(Arrays.toString(list3));
 		return list3;  
 	}
@@ -98,8 +93,48 @@ integer position of the pivot in the newly partitioned array. The original array
 all the elements before the pivot are less than or equal to the pivot and all the elements after the pivot are
 greater than the pivot.*/
 	public static int partition(int[] list) {
+		int divider = list[0] ; 
 		
+		for (int i = 1 ; i < list.length ; i++ ) {
+			swap (list , list[3] , list[0]) ; 
+			divider = list[2] ;
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		System.out.println(divider + " divider") ; 
+		System.out.println(Arrays.toString(list)) ; 
+		return 1 ; 
 	}
 	
-
+	public static void swap(int[] arr , int index1 , int index2)
+	{
+		int x = arr[index1] ;
+		arr[index1] = arr[index2] ;
+		arr[index2] = x ; 
+		System.out.println(x);
+		}	
+		
 }
